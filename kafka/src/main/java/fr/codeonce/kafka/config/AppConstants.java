930962651b34bd4ee830223409e62756b80b2c7d
Mohamed Aziz Hammami <hammamiaziz100@hotmail.com>
2023-06-15 18:56:47 +0100

    first-commit

---
+++ kafka/src/main/java/fr/codeonce/kafka/config/AppConstants.java
@@ -0,0 +1,6 @@
+package fr.codeonce.kafka.config;
+
+public class AppConstants {
+	  public static final String TOPIC_NAME = "testpartition";
+	    public static final String GROUP_ID = "group_id";
+}
